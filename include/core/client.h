27b967392cd91541be05d33917d9d07a218cbab1
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-23 22:51:31 -0700

    Added new command system.

--- include/core/client.h
+++ include/core/client.h
@@ -1,20 +1,79 @@
 #ifndef CORE_SPJALLA_H_
 #define CORE_SPJALLA_H_
 
+#include <functional>
+#include <map>
+#include <tuple>
+
 #include "core/defs.h"
+#include "core/input_line.h"
+#include "core/server.h"
+#include "messages/message.h"
 
 namespace spjalla {
 	class client {
+		using command_handler = std::function<void(pingpong::server_ptr, const input_line &)>;
+		using command_tuple = std::tuple<int, int, command_handler>;
+		using command_pair = std::pair<std::string, command_tuple>;
+
 		private:
 			std::shared_ptr<pingpong::irc> pp;
+			std::multimap<std::string, command_tuple> command_handlers;
 
 		public: 
-			client(std::shared_ptr<pingpong::irc> irc_): pp(irc_) {
-				add_listeners();
+			client(std::shared_ptr<pingpong::irc> irc_): pp(irc_) {}
+
+			/**
+			 * Adds a command handler.
+			 * @param p A pair signifying the name of the command as typed by the user plus a handler tuple.
+			 */
+			client & operator+=(const command_pair &p);
+			
+			/**
+			 * Succinctly adds a handler for a single-argument command.
+			 * @param cmd The name of the command as typed by the user.
+			 */
+			template <typename T>
+			void add(const std::string &cmd) {
+				*this += {cmd, {1, 1, [&](pingpong::server_ptr serv, const input_line &il) {
+					T(serv, il.args[0]).send(true);
+				}}};
 			}
 
-			void input_worker(pingpong::server_ptr serv);
+			/**
+			 * Adds a command handler.
+			 * @param p A pair signifying the name of the command as typed by the user plus a handler tuple.
+			 */
+			void add(const command_pair &p);
+
+			/**
+			 * Initializes the client.
+			 */
+			void init();
+
+			/**
+			 * Reads input from the server socket in an infinite loop and handles data as it comes in.
+			 * @param ptr A pointer to a server.
+			 */
+			void input_worker(pingpong::server_ptr ptr);
+
+			/**
+			 * Processes a line of user input.
+			 * @param  ptr  A pointer to a server.
+			 * @param  line A line of user input.
+			 * @return Whether the line was recognized as a valid input.
+			 */
+			bool handle_line(pingpong::server_ptr ptr, const input_line &line);
+
+			/**
+			 * Adds listeners for pingpong events.
+			 */
 			void add_listeners();
+
+			/**
+			 * Adds the built-in command handlers.
+			 */
+			void add_handlers();
 	};
 }
 
--- src/core/client.cpp
+++ src/core/client.cpp
@@ -20,6 +20,16 @@ using namespace pingpong;
 using namespace spjalla;
 
 namespace spjalla {
+	client & client::operator+=(const command_pair &p) {
+		add(p);
+		return *this;
+	}
+
+	void client::init() {
+		add_listeners();
+		add_handlers();
+	}
+
 	void client::input_worker(server_ptr serv) {
 		std::string in;
 
@@ -28,39 +38,9 @@ namespace spjalla {
 
 			if (il.is_command()) {
 				try {
-					if (il.command == "nick") {
-						if (il.args.size() != 1)
-							YIKES("/nick expects one argument.");
-						nick_command(serv, il.args[0]).send(true);
-					} else if (il.command == "join") {
-						if (il.args.size() != 1)
-							YIKES("/join expects one argument.");
-						join_command(serv, il.args[0]).send(true);
-					} else if (il.command == "quit") {
-						if (il.args.size() == 0) {
-							quit_command(serv).send();
-						} else {
-							quit_command(serv, il.body).send(true);
-						}
-					} else if (il.command == "msg") {
-						if (il.args.size() < 2) {
-							YIKES("/msg expects at least two arguments.");
-						} else {
-							msg_command(serv, il.args[0], il.rest()).send(true);
-						}
-					} else if (il.command == "chans") {
-						std::cout << "Channels:";
-						for (const channel &chan: serv->channels)
-							std::cout << " " << std::string(chan);
-						std::cout << "\n";
-					} else if (il.command == "quote") {
-						serv->quote(il.body);
-					} else if (il.command == "part") {
-						if (il.args.size() < 1)
-							YIKES("/part expects at least one argument.");
-						else
-							part_command(serv, il.args[0], il.rest()).send(true);
-					} else std::cerr << "Unknown command: /" << il.command << std::endl;
+					if (!handle_line(serv, il)) {
+						std::cerr << "Unknown command: /" << il.command << std::endl;
+					}
 				} catch (std::exception &err) {
 					YIKES("Caught an exception (" << typeid(err).name() << "): " << err.what());
 				}
@@ -70,11 +50,64 @@ namespace spjalla {
 		}
 	}
 
+	bool client::handle_line(server_ptr serv, const input_line &il) {
+		const int nargs = static_cast<int>(il.args.size());
+		const std::string &name = il.command;
+
+		auto range = command_handlers.equal_range(name);
+		if (range.first == range.second)
+			return false;
+
+		for (auto it = range.first; it != range.second; ++it) {
+			auto [min, max, fn] = it->second;
+			if (max == 0 && nargs != 0) {
+				YIKES("/" << name << " doesn't accept any arguments.");
+			} else if (min == max && nargs != min) {
+				YIKES("/" << name << " expects " << min << " argument" << (min == 1? "." : "s."));
+			} else if (nargs < min) {
+				YIKES("/" << name << " expects at least " << std::to_string(min) << " argument"
+				      << (min == 1? "." : "s."));
+			} else if (max != -1 && max < nargs) {
+				YIKES("/" << name << " expects at most " << std::to_string(max) << " argument"
+				      << (min == 1? "." : "s."));
+			} else {
+				fn(serv, il);
+			}
+		}
+
+		return true;
+	}
+
+	void client::add(const command_pair &p) {
+		command_handlers.insert(p);
+	}
+
 	void client::add_listeners() {
 		events::listen<message_event>([&](auto *ev) {
 			if (!ev->msg->template is<numeric_message>() && !ev->msg->template is<ping_message>())
 				pp->dbgout() << std::string(*(ev->msg)) << "\n";
 		});
+	}
+
+	void client::add_handlers() {
+		using sptr = pingpong::server_ptr;
+		using line = const input_line &;
+
+		add<nick_command>("nick");
+		add<join_command>("join");
+
+		add({"msg",   {2, -1, [](sptr serv, line il) { msg_command(serv, il.args[0], il.rest()).send(true);  }}});
+		add({"quote", {1, -1, [](sptr serv, line il) { serv->quote(il.body);                                 }}});
+		add({"part",  {1, -1, [](sptr serv, line il) { part_command(serv, il.args[0], il.rest()).send(true); }}});
+		add({"quit",  {0, -1, [](sptr serv, line il) {
+			(il.args.size() == 0? quit_command(serv) : quit_command(serv, il.body)).send(true);
+		}}});
+		add({"chans", {0, 0, [](sptr serv, line) {
+			std::cout << "Channels:";
+			for (const channel &chan: serv->channels)
+				std::cout << " " << std::string(chan);
+			std::cout << "\n";
+		}}});
 	}
 }
 
@@ -82,6 +115,7 @@ int main(int argc, char **argv) {
 	std::shared_ptr<irc> pp = irc::shared();
 	pp->init();
 	client instance(pp);
+	instance.init();
 
 	string hostname;
 
