0f653a15deb82577adb7a0e38b1cc8ed6c4e1c01
Kai Tamkun <ktamkun@ucsc.edu>
2019-05-30 19:35:52 -0700

    Removed haunted test targets

--- src/tests/targets.mk
+++ src/tests/targets.mk
@@ -4,10 +4,3 @@ build/test_boxes: build/tests/test_boxes.o $(COMMONOBJ)
 
 boxtest: build/test_boxes
 	./$^
-
-build/test_haunted: build/tests/test_haunted.o $(COMMONOBJ)
-	@ $(MKBUILD)
-	$(CC) $^ -o $@ $(LDFLAGS) $(LDLIBS)
-
-hauntedtest: build/test_haunted
-	./$^
