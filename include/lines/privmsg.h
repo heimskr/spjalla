cb0f1dcc940a31a075468a85aed8736280bf6222
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-13 03:33:52 -0700

    Implemented CTCP ACTION.

--- include/lines/privmsg.h
+++ include/lines/privmsg.h
@@ -9,17 +9,25 @@
 #include "lines/lines.h"
 
 namespace spjalla::lines {
-	struct privmsg_line: haunted::ui::textline {
-		pingpong::channel_ptr chan;
-		pingpong::user_ptr user;
-		const std::string message;
-		long stamp;
+	class privmsg_line: public haunted::ui::textline {
+		private:
+			/** Returns whether the message is an action (CTCP ACTION). */
+			bool is_action() const;
 
-		privmsg_line(pingpong::channel_ptr chan_, pingpong::user_ptr user_, const std::string &message_, long stamp_);
-		privmsg_line(const pingpong::privmsg_command &cmd):
-			privmsg_line(cmd.destination, cmd.serv->get_user(cmd.serv->get_nick(), true), cmd.message, cmd.sent_time) {}
+			/** Removes the CTCP verb from the message. */
+			std::string trimmed_message() const;
 
-		virtual operator std::string() const override;
+		public:
+			pingpong::channel_ptr chan;
+			pingpong::user_ptr user;
+			const std::string message;
+			long stamp;
+
+			privmsg_line(pingpong::channel_ptr chan_, pingpong::user_ptr user_, const std::string &message_, long stamp_);
+			privmsg_line(const pingpong::privmsg_command &cmd):
+				privmsg_line(cmd.destination, cmd.serv->get_user(cmd.serv->get_nick(), true), cmd.message, cmd.sent_time) {}
+
+			virtual operator std::string() const override;
 	};
 }
 
--- src/lines/privmsg.cpp
+++ src/lines/privmsg.cpp
@@ -3,17 +3,40 @@
 namespace spjalla::lines {
 	privmsg_line::privmsg_line(pingpong::channel_ptr chan_, pingpong::user_ptr user_, const std::string &message_,
 	long stamp_): haunted::ui::textline(0), chan(chan_), user(user_), message(message_), stamp(stamp_) {
-		const std::string start = "[xx:xx:xx] <" + user_->name + "> ";
-		continuation = start.length();
+		if (is_action()) {
+			continuation = ("[xx:xx:xx] <." + user_->name + "> ").length();
+		} else {
+			continuation = ("[xx:xx:xx] * " + user_->name + " ").length();
+		}
+	}
+
+	bool privmsg_line::is_action() const {
+		return !message.empty() && message.find("\1ACTION ") == 0 && message.back() == '\1';
+	}
+
+	std::string privmsg_line::trimmed_message() const {
+		if (message.empty() || message.front() != '\1')
+			return message;
+
+		std::string message_copy {message};
+		if (message_copy.back() == '\1')
+			message_copy.pop_back();
+
+		
+		size_t i, length = message_copy.length();
+		for (i = 0; i < length && message_copy.at(i) != ' '; ++i);
+		return message_copy.at(i) == ' '? message_copy.substr(i + 1) : message_copy;
+		
 	}
 
 	privmsg_line::operator std::string() const {
 		pingpong::hat hat = pingpong::hat::none;
-
 		if (chan->hats.count(user) != 0)
 			hat = chan->hats.at(user);
 
-		return lines::render_time(stamp) + "<"_d + std::string(1, static_cast<char>(hat)) + user->name + "> "_d
-			+ message;
+		const std::string hat_str(1, static_cast<char>(hat));
+
+		return is_action()? (lines::render_time(stamp) + "* "_b + ansi::bold(user->name) + " " + trimmed_message())
+		                  : (lines::render_time(stamp) + "<"_d + hat_str + user->name + "> "_d + message);
 	}
 }
