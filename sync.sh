feadb54cff7cebd641c1d454bf125e34770d177e
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-15 03:00:01 -0700

    Added sync.sh

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Inits and updates submodules. Useful for fresh clones.
+
+git submodule init
+git submodule update
+cd .haunted
+git submodule init
+git submodule update
+cd ../pingpong
+git submodule init
+git submodule update
