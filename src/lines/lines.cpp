6fc74b34396274447760720cc028b20e09e54c92
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-15 00:35:12 -0700

    It was the timestamps!

--- src/lines/lines.cpp
+++ src/lines/lines.cpp
@@ -11,8 +11,8 @@ namespace spjalla::lines {
 
 		std::chrono::system_clock::time_point tpoint {std::chrono::duration<long>(seconds)};
 		std::time_t time = std::chrono::system_clock::to_time_t(tpoint);
-		std::string str(64, '\0');
-		std::strftime(&str[0], str.size(), "%H:%M:%S", std::localtime(&time));
+		char str[64];
+		std::strftime(str, sizeof(str), "%H:%M:%S", std::localtime(&time));
 		return "["_d + str + "] "_d;
 	}
 }
