55b9424b58c5106d2c4ace5a526d58d41581e75a
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-16 04:46:55 -0700

    Keep a copy of the original name in join lines

--- include/lines/join.h
+++ include/lines/join.h
@@ -13,10 +13,11 @@ namespace spjalla::lines {
 	struct join_line: public haunted::ui::textline {
 		pingpong::channel_ptr chan;
 		pingpong::user_ptr user;
+		const std::string name;
 		long stamp;
 
 		join_line(pingpong::channel_ptr chan_, pingpong::user_ptr user_, long stamp_):
-			haunted::ui::textline(0), chan(chan_), user(user_), stamp(stamp_) {}
+			haunted::ui::textline(0), chan(chan_), user(user_), name(user_->name), stamp(stamp_) {}
 
 		join_line(const pingpong::join_event &ev): join_line(ev.chan, ev.who, ev.stamp) {}
 
--- src/lines/join.cpp
+++ src/lines/join.cpp
@@ -2,6 +2,6 @@
 
 namespace spjalla::lines {
 	join_line::operator std::string() const {
-		return lines::render_time(stamp) + "-!- "_d + ansi::bold(user->name) + " joined " + ansi::bold(chan->name);
+		return lines::render_time(stamp) + "-!- "_d + ansi::bold(name) + " joined " + ansi::bold(chan->name);
 	}
 }
