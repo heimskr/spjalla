3001d0c69710c51358269659ad6672e2dac45ad4
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-19 08:05:31 -0700

    Better mode messages

--- include/lines/mode.h
+++ include/lines/mode.h
@@ -1,25 +1,32 @@
 #ifndef SPJALLA_LINES_MODE_H_
 #define SPJALLA_LINES_MODE_H_
 
+#include <unordered_map>
+
 #include "pingpong/events/mode.h"
 
 #include "lines/lines.h"
 
 namespace spjalla::lines {
-	struct mode_line: public haunted::ui::textline {
-		pingpong::modeset mset;
-		std::string where;
-		std::shared_ptr<pingpong::user> who;
-		long stamp;
+	class mode_line: public haunted::ui::textline {
+		private:
+			/** Maps a mode change (e.g., "+o", "-b") to a verb that indicates what the mode change means. */
+			static std::unordered_map<std::string, std::string> verbs;
 
-		mode_line(const pingpong::modeset &mset_, const std::string &where_,
-		const std::shared_ptr<pingpong::user> &who_,long stamp_):
-			haunted::ui::textline(0), mset(mset_), where(where_), who(who_) /*🦉*/, stamp(stamp_) {}
+		public:
+			pingpong::modeset mset;
+			std::string where;
+			std::shared_ptr<pingpong::user> who;
+			long stamp;
 
-		mode_line(const pingpong::mode_event &ev):
-			mode_line(ev.mset, ev.where, ev.who, ev.stamp) {}
+			mode_line(const pingpong::modeset &mset_, const std::string &where_,
+			const std::shared_ptr<pingpong::user> &who_,long stamp_):
+				haunted::ui::textline(0), mset(mset_), where(where_), who(who_) /*🦉*/, stamp(stamp_) {}
 
-		virtual operator std::string() const override;
+			mode_line(const pingpong::mode_event &ev):
+				mode_line(ev.mset, ev.where, ev.who, ev.stamp) {}
+
+			virtual operator std::string() const override;
 	};
 }
 
--- src/lines/mode.cpp
+++ src/lines/mode.cpp
@@ -1,3 +1,5 @@
+#include "core/parse_error.h"
+
 #include "lines/mode.h"
 
 namespace spjalla::lines {
@@ -5,12 +7,38 @@ namespace spjalla::lines {
 		if (!mset.is_type_valid())
 			throw std::logic_error("Unknown mode type: " + std::to_string(static_cast<int>(mset.type)));
 
-		const std::string prefix = lines::render_time(stamp) + lines::notice;
-		const std::string suffix = " ("_d + std::string(mset) + ")"_d;
+		const std::string prefix  {lines::render_time(stamp) + lines::notice};
+		const std::string suffix  {" ("_d + std::string(mset) + ")"_d};
 
 		if (mset.type == pingpong::modeset::mode_type::self)
 			return prefix + "Your usermodes were adjusted" + suffix;
 
-		return prefix + ansi::cyan(who->name) + " set modes for " + ansi::bold(where) + suffix;
+		const std::string modestr = mset.mode_str();
+		const std::string &extra = mset.extra;
+		const std::string styled_name = ansi::cyan(who->name) + " ";
+
+		if (!extra.empty()) {
+			auto iter = verbs.find(modestr);
+			if (iter != verbs.end()) {
+				try {
+					pingpong::mask mask {extra};
+					if (mask.nick == who->serv->get_nick()) {
+						return lines::render_time(stamp) + lines::red_notice + styled_name + iter->second + " " +
+							ansi::bold(extra);
+					}
+				} catch (const pingpong::parse_error &) {}
+
+				return prefix + styled_name + iter->second + " " + ansi::bold(extra);
+			}
+		}
+
+		return prefix + styled_name + " set modes for " + ansi::bold(where) + suffix;
 	}
+
+	std::unordered_map<std::string, std::string> mode_line::verbs = {
+		{"+b", "banned"},    {"-b", "unbanned"},
+		{"+h", "halfopped"}, {"-h", "dehalfopped"},
+		{"+o", "opped"},     {"-o", "deopped"},
+		{"+v", "voiced"},    {"-v", "devoiced"},
+	};
 }
