f04aaa450fc51f725dd8d3d54b129f3bde78e646
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-23 23:51:33 -0700

    Added preliminary key handling code to the history plugin

--- src/plugins/history.cpp
+++ src/plugins/history.cpp
@@ -1,25 +1,38 @@
+#include "haunted/core/key.h"
+
 #include "core/client.h"
 #include "core/plugin_host.h"
 #include "plugins/plugin.h"
 
 #include "formicine/ansi.h"
 
-struct history_plugin: spjalla::plugins::plugin {
-	virtual ~history_plugin() {}
+namespace spjalla::plugins {
+	struct history_plugin: plugin {
+		virtual ~history_plugin() {}
 
-	std::string get_name() const override { return "History"; }
-	std::string get_version() const override { return "0.0.0"; }
+		std::string get_name() const override { return "History"; }
+		std::string get_version() const override { return "0.0.0"; }
 
-	void startup(spjalla::plugins::plugin_host *host) override {
-		spjalla::client *client = dynamic_cast<spjalla::client *>(host);
-		if (!client) {
-			DBG("Error: expected client as plugin host");
-			return;
+		void startup(plugin_host *host) override {
+			spjalla::client *client = dynamic_cast<spjalla::client *>(host);
+			if (!client) {
+				DBG("Error: expected client as plugin host");
+				return;
+			}
+
+			client->handle_pre([&](const haunted::key &key, bool) {
+				if (key == haunted::ktype::up_arrow) {
+					DBG("up arrow");
+					return cancelable_result::disable;
+				} else if (key == haunted::ktype::down_arrow) {
+					DBG("down arrow");
+					return cancelable_result::disable;
+				} else {
+					return cancelable_result::pass;
+				}
+			});
 		}
+	};
+}
 
-
-		DBG("history::startup()");
-	}
-};
-
-history_plugin ext_plugin {};
+spjalla::plugins::history_plugin ext_plugin {};
