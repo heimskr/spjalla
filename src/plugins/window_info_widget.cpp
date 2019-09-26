4255bdd0c21301d0f128b1ddf8ce61c7c72a5bff
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-26 10:27:20 -0700

    Split window info into two widgets

--- src/plugins/window_info_widget.cpp
+++ src/plugins/window_info_widget.cpp
@@ -8,34 +8,59 @@
 #include "formicine/ansi.h"
 
 namespace spjalla::plugins {
-	class window_info_widget: public spjalla::ui::status_widget {
+	class window_info_left_widget: public spjalla::ui::status_widget {
 		private:
 			long stamp = pingpong::util::timestamp();
 
 		public:
 			using status_widget::status_widget;
 
-			virtual ~window_info_widget() {}
+			virtual ~window_info_left_widget() {}
 
 			std::string render(const ui::window *win, bool) const {
 				if (!win) {
 					return "null?";
 				} else if (win->is_status()) {
-					const std::string id = parent->active_server_id();
-					return win->window_name + "] ["_d + (id.empty()? "none"_i : id);
+					return win->window_name;
 				} else if (win->is_channel()) {
-					return parent->active_server_id() + "] ["_d + util::colorize_if_dead(win->data.chan->name, win);
+					return parent->active_server_id();
 				} else if (win->is_user()) {
-					return parent->active_server_id() + "] ["_d + util::colorize_if_dead(win->data.user->name, win);
+					return parent->active_server_id();
 				} else {
 					return ansi::bold(win->window_name);
 				}
 			}
 	};
 
+	class window_info_right_widget: public spjalla::ui::status_widget {
+		private:
+			long stamp = pingpong::util::timestamp();
+
+		public:
+			using status_widget::status_widget;
+
+			virtual ~window_info_right_widget() {}
+
+			std::string render(const ui::window *win, bool) const {
+				if (!win) {
+					return "null?";
+				} else if (win->is_status()) {
+					const std::string id = parent->active_server_id();
+					return id.empty()? "none"_i : id;
+				} else if (win->is_channel()) {
+					return util::colorize_if_dead(win->data.chan->name, win);
+				} else if (win->is_user()) {
+					return util::colorize_if_dead(win->data.user->name, win);
+				} else {
+					return "~";
+				}
+			}
+	};
+
 	class window_info_widget_plugin: public plugin {
 		private:
-			std::shared_ptr<window_info_widget> widget;
+			std::shared_ptr<window_info_left_widget> widget_left;
+			std::shared_ptr<window_info_right_widget> widget_right;
 
 		public:
 			virtual ~window_info_widget_plugin() {}
@@ -52,8 +77,10 @@ namespace spjalla::plugins {
 					return;
 				}
 
-				widget = std::make_shared<window_info_widget>(client, 10);
-				client->add_status_widget(widget);
+				widget_left  = std::make_shared<window_info_left_widget> (client, 10);
+				widget_right = std::make_shared<window_info_right_widget>(client, 15);
+				client->add_status_widget(widget_left);
+				client->add_status_widget(widget_right);
 			}
 	};
 }
