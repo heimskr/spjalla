848e6be5564d6240cbb7b0aef341b08fb312b0f9
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-28 00:24:11 -0700

    More config tests

--- src/tests/test_config.cpp
+++ src/tests/test_config.cpp
@@ -7,11 +7,24 @@ int main(int, char **) {
 
 namespace spjalla::tests {
 	void test_config(haunted::tests::testing &unit) {
+		using namespace std::string_literals;
+
 		config cfg;
-		unit.check(cfg.registered.size(), 0UL, "registered.count()");
+		config::groupmap &reg = cfg.registered;
+
+		unit.check(reg.size(), 0UL, "registered.count()");
 		cfg.register_key("group", "one", {"string"});
-		unit.check(cfg.registered.size(), 1UL, "registered.count()");
-		unit.check(cfg.registered.at("group").size(), 1UL, "group.count()");
-		unit.check(cfg.registered.at("group").at("one").get_type(), config_type::string_, "group.one.type");
+		unit.check(reg.size(), 1UL, "registered.count()");
+
+		config::submap &group = reg.at("group");
+		unit.check(group.size(), 1UL, "group.count()");
+
+		config_value &one = group.at("one");
+		unit.check(one.get_type(), config_type::string_, "group.one.type");
+
+		unit.check({
+			{{"foo=bar"s}, {"foo", "bar"}},
+			{{" foo = bar "s}, {"foo", "bar"}},
+		}, &config::parse_kv_pair, "config::parse_kv_pair");
 	}
 }
