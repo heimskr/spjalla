9b4f495f16b455c81c2953284a477841c9b8d564
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-27 21:40:19 -0700

    Added utility functions for getting the home directory.

--- include/core/sputil.h
+++ include/core/sputil.h
@@ -1,5 +1,5 @@
+#include <filesystem>
 #include <string>
-
 
 namespace spjalla {
 	namespace ui {
@@ -9,5 +9,11 @@ namespace spjalla {
 	namespace util {
 		/** Styles a window based on whether it's dead. */
 		std::string colorize_if_dead(const std::string &, const ui::window *);
+
+		/** Returns a path to the user's home directory as a string ending with a slash. */
+		std::string get_home_string();
+
+		/** Returns a path to the user's home directory. */
+		std::filesystem::path get_home();
 	}
 }
--- src/core/sputil.cpp
+++ src/core/sputil.cpp
@@ -1,8 +1,28 @@
+#include <cstdlib>
+
+#include <pwd.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "core/sputil.h"
 #include "ui/window.h"
 
 namespace spjalla::util {
 	std::string colorize_if_dead(const std::string &str, const ui::window *win) {
 		return win->data.dead? ansi::red(str) : str;
+	}
+
+	std::string get_home_string() {
+		const char *home = getenv("HOME");
+		if (home == nullptr)
+			home = getpwuid(getuid())->pw_dir;
+		std::string str {home};
+		if (!str.empty() && str.back() != '/' && str.back() != '\\')
+			str.push_back('/');
+		return str;
+	}
+
+	std::filesystem::path get_home() {
+		return {get_home_string()};
 	}
 }
