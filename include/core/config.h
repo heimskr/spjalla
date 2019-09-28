e796fe6393a9ee1182270e201fcbe1f9c9cbb467
Kai Tamkun <ktamkun@ucsc.edu>
2019-09-27 21:40:32 -0700

    Beginning work on the config system.

--- include/core/config.h
+++ include/core/config.h
@@ -1,11 +1,43 @@
 #ifndef SPJALLA_CORE_CONFIG_H_
 #define SPJALLA_CORE_CONFIG_H_
 
+#include <string>
+#include <utility>
+
 #include "haunted/core/key.h"
 
-namespace spjalla::config {
+namespace spjalla {
 	struct keys {
 		static haunted::key toggle_overlay, switch_server, next_window, previous_window;
+	};
+
+	class config {
+		private:
+			/** Creates a config directory in the user's home directory if one doesn't already exist.
+			 *  Returns true if the directory had to be created. */
+			static bool ensure_config_dir(const std::string &name = ".spjalla");
+
+			/** Attempts to parse a keyvalue pair of the form /^(\w+)=(.+)$/. */
+			static std::pair<std::string, std::string> parse_kv_pair(const std::string &);
+
+		public:
+			enum class line_type {long_line, double_line, string_line};
+
+			/** Escapes a string by prepending all backslashes, newlines, carriage returns, tabs, nulls and double
+			 *  quotes with backslashes. */
+			static std::string escape(const std::string &);
+
+			/** Unescapes a string (see spjalla::config::escape). */
+			static std::string unescape(const std::string &);
+
+			/** Attempts to parse a configuration line of the form /^\w+\s*=\s*\d+$/. */
+			static std::pair<std::string, long> parse_long_line(const std::string &);
+
+			/** Attempts to parse a configuration line of the form /^\w+\s*=\s*\d+\.\d*$/. */
+			static std::pair<std::string, double> parse_double_line(const std::string &);
+
+			/** Attempts to parse a configuration line of the form /^\w+\s*=\s*"[^\\\n\r\t\0"]*"$/. */
+			static std::pair<std::string, std::string> parse_string_line(const std::string &);
 	};
 }
 
--- src/core/config.cpp
+++ src/core/config.cpp
@@ -1,8 +1,88 @@
-#include "core/config.h"
+#include <sstream>
 
-namespace spjalla::config {
+#include "core/config.h"
+#include "core/sputil.h"
+
+namespace spjalla {
 	haunted::key keys::toggle_overlay  = {haunted::ktype::semicolon, haunted::kmod::ctrl};
 	haunted::key keys::switch_server   = {haunted::ktype::x,         haunted::kmod::ctrl};
 	haunted::key keys::next_window     = {haunted::ktype::n,         haunted::kmod::ctrl};
 	haunted::key keys::previous_window = {haunted::ktype::p,         haunted::kmod::ctrl};
+
+	bool config::ensure_config_dir(const std::string &name) {
+		std::filesystem::path config_path = util::get_home() / name;
+		if (!std::filesystem::exists(config_path)) {
+			std::filesystem::create_directory(config_path);
+			return true;
+		}
+
+		return false;
+	}
+
+	std::pair<std::string, std::string> config::parse_kv_pair(const std::string &str) {
+		if (str.empty())
+			throw std::invalid_argument("Can't parse empty string as key-value pair");
+
+		const size_t equals = str.find('=');
+
+		if (equals == std::string::npos)
+			throw std::invalid_argument("No equals sign found in key-value pair");
+
+		if (equals == 0)
+			throw std::invalid_argument("Empty key in key-value pair");
+
+		if (equals == str.length() - 1)
+			throw std::invalid_argument("Empty value in key-value pair");
+
+		return {str.substr(0, equals), str.substr(equals + 1)};
+	}
+
+	std::string config::escape(const std::string &str) {
+		std::ostringstream out;
+		for (char ch: str) {
+			switch (ch) {
+				case '\\': out << "\\\\"; break;
+				case '\n': out << "\\n"; break;
+				case '\r': out << "\\r"; break;
+				case '\t': out << "\\t"; break;
+				case '\0': out << "\\0"; break;
+				case '"':  out << "\""; break;
+				default:   out << ch;
+			}
+		}
+
+		return out.str();
+	}
+
+	std::string config::unescape(const std::string &str) {
+		std::ostringstream out;
+		for (size_t i = 0, length = str.length(); i < length; ++i) {
+			char ch = i;
+
+			// Looking at the next character when we're at the end of the string would be bad.
+			if (i == length - 1) {
+				out << ch;
+				break;
+			}
+
+			if (ch == '\\') {
+				switch (str[i + 1]) {
+					case '\\': out << "\\"; ++i; break;
+					case 'n':  out << "\n"; ++i; break;
+					case 'r':  out << "\r"; ++i; break;
+					case 't':  out << "\t"; ++i; break;
+					case '0':  out << "\0"; ++i; break;
+					case '"':  out << "\""; ++i; break;
+				}
+			} else {
+				out << ch;
+			}
+		}
+
+		return out.str();
+	}
+
+	std::pair<std::string, long> config::parse_long_line(const std::string &str) {
+
+	}
 }
