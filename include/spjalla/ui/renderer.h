e100354facc698f8ec6aed4d7d26d6924a189378
Kai Tamkun <ktamkun@ucsc.edu>
2019-11-06 19:07:35 -0800

    Added rendering providers

--- include/spjalla/ui/renderer.h
+++ include/spjalla/ui/renderer.h
@@ -13,11 +13,32 @@ namespace spjalla::ui {
 	 * Handles rendering of small bits of the interface, such as nicks in messages and channel names.
 	 */
 	class renderer {
+		public:
+			using fn_str         = std::function<std::string(const std::string &)>;
+			using fn_nick_str    = std::function<std::string(const std::string &, bool)>;
+			using fn_channel_ptr = std::function<std::string(std::shared_ptr<pingpong::channel>)>;
+			using fn_nick_ptr =
+				std::function<std::string(std::shared_ptr<pingpong::user>, std::shared_ptr<pingpong::channel>, bool)>;
+
 		private:
 			config::cache *cache;
 
+			fn_nick_str    provider_nick_str;
+			fn_nick_ptr    provider_nick_ptr;
+			fn_str         provider_channel_str;
+			fn_channel_ptr provider_channel_ptr;
+
 		public:
+			/**
+			 * normal:  In most places, like join messages.
+			 * message: At the beginning of messages.
+			 */
+			enum class nick_place {normal, message};
+
 			renderer(config::cache &cache_): cache(&cache_) {}
+
+
+// Wrappers
 
 			/** Formats a nick according to interface.nick_format. */
 			std::string nick(const std::string &, bool bright = false) const;
@@ -31,6 +52,23 @@ namespace spjalla::ui {
 
 			/** Formats a channel name according to interface.channel_format. */
 			std::string channel(std::shared_ptr<pingpong::channel>) const;
+
+
+// Implementations
+
+			std::string nick_impl(const std::string &, bool bright = false) const;
+			std::string nick_impl(std::shared_ptr<pingpong::user>, std::shared_ptr<pingpong::channel>,
+				bool bright = false) const;
+			std::string channel_impl(const std::string &) const;
+			std::string channel_impl(std::shared_ptr<pingpong::channel>) const;
+
+
+// Providers
+
+			void provide_nick(fn_nick_str);
+			void provide_nick(fn_nick_ptr);
+			void provide_channel(fn_str);
+			void provide_channel(fn_channel_ptr);
 	};
 }
 
--- src/ui/renderer.cpp
+++ src/ui/renderer.cpp
@@ -2,7 +2,12 @@
 #include "spjalla/ui/renderer.h"
 
 namespace spjalla::ui {
-	std::string renderer::nick(const std::string &nick, bool bright) const {
+
+
+// Implementations
+
+
+	std::string renderer::nick_impl(const std::string &nick, bool bright) const {
 		std::string format = bright? cache->interface_nick_format_bright : cache->interface_nick_format;
 
 		const size_t nick_pos = format.find("#n");
@@ -14,8 +19,8 @@ namespace spjalla::ui {
 		return ansi::format(format);
 	}
 
-	std::string renderer::nick(std::shared_ptr<pingpong::user> user,
-	                                   std::shared_ptr<pingpong::channel> chan, bool bright) const {
+	std::string renderer::nick_impl(std::shared_ptr<pingpong::user> user, std::shared_ptr<pingpong::channel> chan,
+	                                bool bright) const {
 		std::string format = bright? cache->interface_nick_format_bright : cache->interface_nick_format;
 
 		const size_t hat_pos = format.find("#h");
@@ -33,19 +38,50 @@ namespace spjalla::ui {
 		return ansi::format(format);
 	}
 
-	std::string renderer::channel(const std::string &channel) const {
+	std::string renderer::channel_impl(const std::string &chan) const {
 		std::string format = cache->interface_channel_format;
 
 		const size_t channel_pos = format.find("#c");
 		if (channel_pos != std::string::npos) {
 			format.erase(channel_pos, 2);
-			format.insert(channel_pos, channel);
+			format.insert(channel_pos, chan);
 		}
 
 		return ansi::format(format);
 	}
 
-	std::string renderer::channel(std::shared_ptr<pingpong::channel> chan) const {
+	std::string renderer::channel_impl(std::shared_ptr<pingpong::channel> chan) const {
 		return channel(chan->name);
 	}
+
+
+// Wrappers
+
+
+	std::string renderer::nick(const std::string &nick, bool bright) const {
+		return provider_nick_str? provider_nick_str(nick, bright) : nick_impl(nick, bright);
+	}
+
+	std::string renderer::nick(std::shared_ptr<pingpong::user> user, std::shared_ptr<pingpong::channel> chan,
+	                                bool bright) const {
+		return provider_nick_ptr? provider_nick_ptr(user, chan, bright) : nick_impl(user, chan, bright);
+	}
+
+	std::string renderer::channel(const std::string &chan) const {
+		return provider_channel_str? provider_channel_str(chan) : channel_impl(chan);
+	}
+
+	std::string renderer::channel(std::shared_ptr<pingpong::channel> chan) const {
+		return provider_channel_ptr? provider_channel_ptr(chan) : channel_impl(chan);
+	}
+
+
+// Providers
+
+
+	void renderer::provide_nick(renderer::fn_nick_str fn) { provider_nick_str = fn; }
+	void renderer::provide_nick(renderer::fn_nick_ptr fn) { provider_nick_ptr = fn; }
+	void renderer::provide_channel(renderer::fn_str         fn) { provider_channel_str = fn; }
+	void renderer::provide_channel(renderer::fn_channel_ptr fn) { provider_channel_ptr = fn; }
+
 }
