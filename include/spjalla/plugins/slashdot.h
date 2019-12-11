c2c908d57d1053025617f0c070a04f4cc6b6b6fe
Kai Tamkun <ktamkun@ucsc.edu>
2019-12-10 19:18:43 -0800

    XML parsing :)

---
+++ include/spjalla/plugins/slashdot.h
@@ -0,0 +1,38 @@
+#ifndef SPJALLA_PLUGINS_SLASHDOT_H_
+#define SPJALLA_PLUGINS_SLASHDOT_H_
+
+#include "spjalla/plugins/plugin.h"
+
+namespace tinyxml2 {
+	class XMLElement;
+};
+
+namespace spjalla::plugins {
+	namespace slashdot {
+		struct story {
+			std::string title, url, author, department, section;
+			int comments; 
+		};
+
+		class parser {
+			private:
+				std::string get_text(tinyxml2::XMLElement *);
+			public:
+				std::vector<story> stories {};
+				void parse(const std::string &);
+		};
+	}
+
+	struct slashdot_plugin: public plugin {
+		~slashdot_plugin() {}
+
+		std::string get_name()        const override { return "Slashdot"; }
+		std::string get_description() const override { return "Adds a Slashdot reader."; }
+		std::string get_version()     const override { return "0.0.1"; }
+
+		void postinit(plugin_host *host) override;
+		void cleanup(plugin_host *) override;
+	};
+}
+
+#endif
--- src/plugins/slashdot/targets.mk
+++ src/plugins/slashdot/targets.mk
@@ -1,6 +1,6 @@
 build/plugins/slashdot/%.o: src/plugins/slashdot/%.cpp
 	@ mkdir -p "$(shell dirname "$@")"
-	$(CC) $(strip $(ALLFLAGS) $(INCLUDE_SP) $(INCLUDE_LIBS) $(CFLAGS_BOOST)) -c $< -o $@
+	$(CC) $(strip $(ALLFLAGS) $(INCLUDE_SP) $(INCLUDE_LIBS) $(CFLAGS_TINYXML2)) -c $< -o $@
 
 build/plugins/slashdot.dylib: $(patsubst %.cpp,%.o,$(addprefix build/plugins/slashdot/,$(filter %.cpp,$(shell ls "src/plugins/slashdot")))) $(MAINLIB)
-	$(CC) $(SHARED_FLAG) $+ -o $@ $(LDFLAGS) $(LDFLAGS_BOOST)
+	$(CC) $(SHARED_FLAG) $+ -o $@ $(LDFLAGS) $(LD_TINYXML2)
