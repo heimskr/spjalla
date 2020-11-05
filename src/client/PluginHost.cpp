e368c5632ebd00446325c37ec830be4d85821bb5
Kai Tamkun <ktamkun@ucsc.edu>
2020-11-04 21:11:15 -0800

    Forgot to move Makefile changes back into configure

--- src/client/PluginHost.cpp
+++ src/client/PluginHost.cpp
@@ -31,7 +31,7 @@ namespace Spjalla::Plugins {
 
 		void *lib = dlopen(path.c_str(), RTLD_NOW | RTLD_LOCAL);
 		if (lib == nullptr)
-			throw std::runtime_error("dlopen returned nullptr");
+			throw std::runtime_error("dlopen returned nullptr. " + std::string(dlerror()));
 		
 		Plugins::Plugin *plugin = static_cast<Plugins::Plugin *>(dlsym(lib, PLUGIN_GLOBAL_VARIABLE_NAME));
 		if (plugin == nullptr)
